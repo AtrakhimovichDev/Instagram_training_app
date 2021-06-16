6fc1cfa294e65c90e4d7f38d24921d3dbfca53e4
Andrei <atrakhimovich95@gmail.com>
2021-06-16 08:36:19 +0300

    Initial Commit

---
+++ Homework_Gallery_2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_Gallery_2.0
+//
+//  Created by Andrei Atrakhimovich on 16.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
